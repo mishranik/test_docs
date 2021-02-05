917cc1331cef822b7a7ccaca288a9c6b12dc5abf
mishranik <nikhilmishra@moengage.com>
2021-02-05 20:20:52 +0530

    Hello, my col frien

--- moengage/app.py
+++ moengage/app.py
@@ -4,6 +4,6 @@ class App(object):
         """
          A method that speaks to your refrigerator
         :param erlich: are you erlich?
-        :return: string that speaks jin yang
+        :return: Hello, my col frien
         """
         return 'Hello, my col frien'
