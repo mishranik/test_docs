c01370624ec3ff7771c9190988865d7f8cf8d67a
mishranik <nikhilmishra@moengage.com>
2021-02-05 16:18:03 +0530

    intial setup of docs

---
+++ moengage/app.py
@@ -0,0 +1,7 @@
+def hello(erlich=False):
+    """
+     A method that speaks to your refrigerator
+    :param erlich: are you erlich?
+    :return: string that speaks jin yang
+    """
+    return 'Hello, my col frien'
---
+++ tests/app.py
@@ -0,0 +1,4 @@
+from moengage import app
+
+def test_app():
+    assert app.hello() == 'Hello, my col frien'
